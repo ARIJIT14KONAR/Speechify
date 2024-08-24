58f03005687f9101f5c974e32823e127169f60f4
ARIJIT14KONAR <konararijit14@gmail.com>
2024-08-24 20:32:51 +0530

    keep it up

--- app.py
+++ app.py
@@ -14,15 +14,13 @@ def index():
 def convert_to_speech():
     text = request.form['text']
     tts = gTTS(text=text, lang='en')
-    filename = f"{uuid4()}.mp3"
-    filepath = os.path.join("static", filename)
-    tts.save(filepath)
-    
-    
+    file_io = io.BytesIO()
+    tts.write_to_fp(file_io)
+    file_io.seek(0)
 
-    return send_file(filepath, as_attachment=True)
+    return send_file(file_io, as_attachment=True, download_name=f"{uuid4()}.mp3", mimetype='audio/mpeg')
 
-#if __name__ == '__main__':
-#    if not os.path.exists("static"):
-#        os.makedirs("static")
-#    app.run(debug=False)
+if __name__ == '__main__':
+    if not os.path.exists("static"):
+        os.makedirs("static")
+    app.run(debug=False)
--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from app import app
 if __name__ == '__main__':
     if not os.path.exists("static"):
         os.makedirs("static")
-    app.run(debug=True)
+    app.run(debug=False)
